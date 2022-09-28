f2d4a4661a5ec1721c1a8a130d5d155acc1b9fc9
Juan Manuel Arboleda Taborda <jmarboledat@unal.edu.co>
2022-09-27 20:58:19 -0500

    controler de sitter hecho

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -1,2 +1,49 @@
 class SittersController < ApplicationController
+  before_action :set_sitter, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @sitters = Sitter.all
+  end
+
+  def show; end
+
+  def new
+    @sitter = Sitter.new
+  end
+
+  def create
+    @sitter = Sitter.new(sitter_params)
+    @sitter.user = current_user
+    if @sitter.save
+      redirect_to sitter_path(@sitter)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    @sitter.update(sitter_params)
+    if @sitter.save
+      redirect_to sitter_path(@sitter)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @sitter.destroy
+  end
+
+  private
+
+  def set_sitter
+    @sitter = Sitter.find(params[:id])
+  end
+
+  def sitter_params
+    params.require(:sitter).permit(:end_date, :start_date)
+  end
 end
