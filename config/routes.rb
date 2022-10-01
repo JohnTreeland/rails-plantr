4fd97f3d2ec27d282af4fb2d53ea156541d48ea1
Juan Manuel Arboleda Taborda <jmarboledat@unal.edu.co>
2022-10-01 14:37:40 -0500

    last version

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
-    resources :sitters, only: %i[new create edit
+    resources :sitters, only: %i[new create edit]
   end
 
   patch "sitters/:id", to: "sitters#update", as: :update_sitter
