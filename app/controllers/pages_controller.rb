07437374d88f21d2d8fb0adeef0d391d32cfa0a2
romariomestra <romal-17@hotmail.com>
2022-10-04 21:00:22 -0500

    funciona search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if params[:query].present?
+      @sitters = Sitter.where(city: params[:query])
+    else
+      @sitters = Sitter.all
+    end
   end
 end
